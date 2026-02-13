8f5843c3fd609d94231191fdd6cc921728bddcf0
Tomasz <kwp@tlen.pl>
2026-02-13 14:30:39 +0000

    6 commit

--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,7 @@ variable "ecr_repo_name" {
   type        = string
   default = "tk-demo-app"
 }
+
+variable "app_image" {
+  type = string
+  default = "504913911906.dkr.ecr.eu-central-1.amazonaws.com/tk-demo-app:5"
