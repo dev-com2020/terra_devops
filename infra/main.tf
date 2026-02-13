28dce3fd54a0a9b999165454a0046e93fbc98e33
Tomasz <kwp@tlen.pl>
2026-02-13 14:45:18 +0000

    10 commit

--- infra/main.tf
+++ infra/main.tf
@@ -12,6 +12,43 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_availability_zones" "az" {}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  tags = { Name = "tk-demo-vpc" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "tk-demo-igw" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.10.1.0/24"
+  availability_zone       = data.aws_availability_zones.az.names[0]
+  map_public_ip_on_launch = true
+  tags = { Name = "tk-demo-public" }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = { Name = "tk-demo-public-rt" }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_ecr_repository" "demo" {
   name                 = var.ecr_repo_name
   image_tag_mutability = "MUTABLE"
